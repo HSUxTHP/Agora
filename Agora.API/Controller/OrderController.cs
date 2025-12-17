e0e68a641a21cfc1a916c83ee9a78f08b9a3b19b
SAFxTHP <hoangphi9.8lct1@gmail.com>
2025-12-17 23:47:10 +0700

    Add user authorization and user ID retrieval in OrderController

--- Agora.API/Controller/OrderController.cs
+++ Agora.API/Controller/OrderController.cs
@@ -2,10 +2,13 @@ using System.Threading.Tasks;
 using Agora.Application.DTOs;
 using Agora.Application.Service;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Agora.API.Controller
 {
     [Route("api/[controller]")]
+    [Authorize] 
     [ApiController]
     public class OrderController : ControllerBase
     {
@@ -16,11 +19,23 @@ namespace Agora.API.Controller
             _orderService = orderService;
         }
 
+        private int GetUserId()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                throw new UnauthorizedAccessException("User ID not found in token.");
+            }
+            return int.Parse(userIdClaim.Value);
+        }
+
         [HttpPost("checkout")]
         public async Task<IActionResult> Checkout([FromBody] CheckoutRequest request)
         {
             try
             {
+                int userId = GetUserId();
+                request.UserId = userId;
                 var result = await _orderService.CheckoutAsync(request);
                 return Ok(result);
             }
@@ -29,5 +44,7 @@ namespace Agora.API.Controller
                 return BadRequest(new { message = ex.Message });
             }
         }
+
+        //TODO: Add GetOrders endpoint
     }
 }
